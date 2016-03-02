dafec6a95999d90e2db242d28a4be72cd517b789
Bobby Allen <ballen@bobbyallen.me>
2016-03-02 19:33:32 +0000

    Adding missing source headers.

--- src/Version.php
+++ src/Version.php
@@ -2,6 +2,17 @@
 
 namespace Ballen\GitVersionNumber;
 
+/**
+ * Git Version Number
+ * 
+ * A library for extracting and utilising your project's Git version information.
+ *
+ * @author Bobby Allen <ballen@bobbyallen.me>
+ * @license https://raw.githubusercontent.com/bobsta63/git-version-number/master/LICENSE
+ * @link https://github.com/bobsta63/git-version-number
+ * @link http://www.bobbyallen.me
+ *
+ */
 class Version
 {
 
@@ -151,4 +162,4 @@ class Version
         }
         return $version;
     }
-}Ï
+}
--- src/VersionFactory.php
+++ src/VersionFactory.php
@@ -2,6 +2,17 @@
 
 namespace Ballen\GitVersionNumber;
 
+/**
+ * Git Version Number
+ * 
+ * A library for extracting and utilising your project's Git version information.
+ *
+ * @author Bobby Allen <ballen@bobbyallen.me>
+ * @license https://raw.githubusercontent.com/bobsta63/git-version-number/master/LICENSE
+ * @link https://github.com/bobsta63/git-version-number
+ * @link http://www.bobbyallen.me
+ *
+ */
 class VersionFactory
 {
 
@@ -14,5 +25,4 @@ class VersionFactory
     {
         return new Version($path);
     }
-
 }
