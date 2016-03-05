86078e5a1e85bc411524abf692644119a3c1585b
Bobby Allen <ballen@bobbyallen.me>
2016-03-05 12:49:55 +0000

    Adds test for VersionFactory class and adds a Project Stub directory.

---
+++ tests/VersionFactoryTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit_Framework_TestCase;
+use Ballen\GitVersionNumber\Version;
+use Ballen\GitVersionNumber\VersionFactory;
+
+class CollectionModelTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testFactoryReturnsVersionInstance()
+    {
+        $this->assertInstanceOf(Version::class, VersionFactory::create('../'));
+    }
+}
